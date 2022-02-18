a3edc3cabf7fc7dc9d146af961f55eb8b9ede303
Fabian Speh <fabian.speh@mni.thm.de>
2022-02-18 11:55:24 +0100

    ability to create posts, new board controller/view that shows all posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'board/index'
   resources :posts
-  root to: 'posts#index'
+  root to: 'board#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
