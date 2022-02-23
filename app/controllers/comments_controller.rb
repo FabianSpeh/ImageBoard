26828c33c9fc56fd8a4eca516363145bd1abda0b
Fabian Speh <fabian.speh@mni.thm.de>
2022-02-23 22:57:37 +0100

    added functioanlity to delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,19 @@ def destroy
     redirect_to post_path(params[:post_id])
 end
 
+def edit
+    @comment = Comment.find(params[:id])
+    
+end
+
+def update
+
+     @comment = Comment.find(params[:id])
+     puts(params[:body])
+     @comment.update(body: params[:comment][:body])
+     redirect_to post_path(params[:post_id])
+end
+
 
     def comment_params
         params.require(:comment).permit(:body).merge(post_id: params[:post_id])
